7c33c751316d4ba91d0bfba3244f1ea9fadf67cd
RAJ- UserBoT <134812025+MrHacker5575@users.noreply.github.com>
2023-07-21 13:19:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for SESSION in SESSIONS:
             session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
-            plugins=dict(root="TheXSpam"),
+            plugins=dict(root="CoDeX"),
         )
         CLIENTS.append(client)
 
@@ -20,11 +20,11 @@ if __name__ == "__main__":
     for i, CLIENT in enumerate(CLIENTS):
         try:
             CLIENT.start()
-            CLIENT.join_chat("TheAltron")
-            CLIENT.join_chat("AltronChats")
+            CLIENT.join_chat("Heart_Connection")
+            CLIENT.join_chat("Heart_Connection")
             print(f"---> Client {i+1} has been Started...")
         except Exception as e:
             print(e)
 
-    print("💞YOUR PY-ALTRON SPAM USERBOTS DEPLOYED SUCCESSFULLY 💞")
+    print("🔥💫𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐔𝐬𝐞𝐫𝐁𝐨𝐭❌ 𝐌𝐘_𝐌𝐀𝐒𝐓𝐄𝐑--⚡🇨Ⓞ𝗗𝜩🇽⚡")
     idle()
